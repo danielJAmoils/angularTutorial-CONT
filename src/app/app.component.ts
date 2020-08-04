720802ee0b850b4a004d517feeac600a09134e4d
Daniel Amoils <danieljamoils@gmail.com>
2020-08-04 18:14:33 -0400

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  private actualAmountUSD = 50
+  INR = true
+
+  get amount(){
+    return this.INR ? this.actualAmountUSD * 75 : this.actualAmountUSD
+  }
+
+  get format(){
+    return this.INR ? 'INR' : 'USD'
+  }
+
+  toggleCurrency(){
+    this.INR = !this.INR
+  }
 }
