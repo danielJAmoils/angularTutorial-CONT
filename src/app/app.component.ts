f5f4659ade65f11c840cb2a41e336bdb531868d0
Daniel Amoils <danieljamoils@gmail.com>
2020-08-04 19:27:32 -0400

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private actualAmountUSD = 50
-  INR = true
-
-  get amount(){
-    return this.INR ? this.actualAmountUSD * 75 : this.actualAmountUSD
-  }
-
-  get format(){
-    return this.INR ? 'INR' : 'USD'
-  }
-
-  toggleCurrency(){
-    this.INR = !this.INR
+  someRandomVariable = false
+  data = [
+    {
+      lang: 'JavaScript',
+      usedOn: 'web'
+    },
+    {
+      lang: 'Swift',
+      usedOn: 'ios'
+    }
+  ]
+  constructor() {
+    setInterval(() => {
+      this.someRandomVariable = !this.someRandomVariable
+    }, 1000)
   }
 }
